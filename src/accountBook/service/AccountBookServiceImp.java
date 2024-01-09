98d28d52d4e62d818856291294ca39f202c7db4b
sonnayeoung <sskss00@naver.com>
2024-01-09 09:34:12 +0900

    가계부 : 가이드라인 (개인과제 부여)

--- src/accountBook/service/AccountBookServiceImp.java
+++ src/accountBook/service/AccountBookServiceImp.java
@@ -6,30 +6,36 @@ import accountBook.AccountBook;
 
 public class AccountBookServiceImp implements AccountBookService{
 
+	/**1. 가계부(리스트)에 내역을 추가하는 메소드 :  심아진*/
 	@Override
 	public boolean addAB(List<AccountBook> list, AccountBook ab) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+	
+	/**2. 가계부(리스트)에 내역을 조회하는 메소드 :  신경재*/
 	@Override
 	public boolean printAB(List<AccountBook> list, String title) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+	/**3. 가계부(리스트)에 내역을 수정하는 메소드 :  손나영*/
 	@Override
 	public boolean setAB(List<AccountBook> list, String title, int moeny) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+	/**4. 가계부(리스트)에 내역을 삭제하는 메소드 :  양선진*/
 	@Override
 	public boolean deleteAB(List<AccountBook> list, String title) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+	/**5. 현재 잔액을 출력하는 메소드 :  신경재*/
 	@Override
 	public boolean printCurrentMoney() {
 		// TODO Auto-generated method stub
