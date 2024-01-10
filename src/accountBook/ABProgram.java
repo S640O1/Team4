4d2e094074c47af914a2339e51d50197db157b7a
sonnayeoung <sskss00@naver.com>
2024-01-10 19:26:26 +0900

    병합

--- src/accountBook/ABProgram.java
+++ src/accountBook/ABProgram.java
@@ -1,6 +1,11 @@
 package accountBook;
 
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
@@ -16,6 +21,8 @@ import program.Program;
 public class ABProgram implements Program{
 	static String fileName = "src/accountBook/accountBookList.txt";
 	private Scanner scan = new Scanner(System.in);
+	SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd");
+
 	
 	//서비스 불러오기
 	private AccountBookService accountBookService = new AccountBookServiceImp();
@@ -35,7 +42,8 @@ public class ABProgram implements Program{
 	public void run() {
 		int menu = 0;
 		
-//		load(fileName);
+		List<Item> list = fileService.load(fileName);
+		
 		do {
 			printMenu();
 			try {
@@ -46,8 +54,17 @@ public class ABProgram implements Program{
 				scan.nextLine();
 			}
 		} while (menu != EXIT);
-//		save(fileName);
+		
+		if(fileService.save(fileName, list)) {
+			System.out.println("저장"); 
+			
+		};
+		
+
 	}
+	
+
+	
 
 	@Override
 	public void printMenu() {
@@ -80,45 +97,48 @@ public class ABProgram implements Program{
 
 	/** 가계부 입력 : 심아진 */
 	private void insertMoney() {
-		// TODO Auto-generated method stub
+		if(!accountBookService.addAB(list, fileName)) {
+			System.out.println("입력에 실패했습니다.");
+		}
 		
 	}
 
 	/** 가계부 조회 : 경재*/
 	private void printMoney() {
-		// TODO Auto-generated method stub
-		
+		if(!accountBookService.printAB(list)) {
+			System.out.println("조회에 실패했습니다.");
+		}
 	}
 
 
 	/** 가계부 수정 : 손나영 */
 	private void updateMoney() {
 		//list가 비어있으면 
-		if(!accountBookService.isList()) {
-			System.out.println("작성된 가계부가 없습니다.");
-			return;
-		}
-		
+//		if(!accountBookService.isList()) {
+//			System.out.println("작성된 가계부가 없습니다.");
+//			return;
+//		}
+//		
 		//전체목록 보여줌
-		for(int i=0; i<list.size(); i++) {
-			list.get(i).toString(i);
+		if(!accountBookService.printAB(list)) {
+			System.out.println("조회에 실패했습니다.");
 		}
 		
 		int index=-1;
 		//수정할 항목 받아오기
 		try {
 			System.out.print("어떤 항목을 수정하시겠습니까? : "); 
-			index = scan.nextInt() -1;
+			index = scan.nextInt()-1;
 		}catch (InputMismatchException e){
 			System.out.println("잘못된 메뉴입니다.");
 			scan.nextLine();
 		}
 		
 		//index가 잘못된 경우
-		if(!accountBookService.indexError(index)) {
-			System.out.println("없는 항목입니다.");
-			return;			
-		}
+//		if(!accountBookService.indexError(index)) {
+//			System.out.println("없는 항목입니다.");
+//			return;			
+//		}
 		
 		//메뉴 선택
 		int menu = 0;
@@ -170,9 +190,12 @@ public class ABProgram implements Program{
 
 	/** 가계부 수정 6. 전체수정*/
 	private void runUpateInAll(int index) {
-		int money = 0, totalMoney = 0, date = 000000, classify;
+		int money = 0, totalMoney = 0, classify;
+		Date date = null;
 		boolean outMoney = false, inMoney = false;
 		String memo = null;
+		
+		
 		//전체 수정 항목 받아오기
 		try {
 			System.out.print("입금(1)/지출(2) : ");
@@ -187,7 +210,11 @@ public class ABProgram implements Program{
 				System.out.println("잘못된 번호입니다.");
 			}
 			System.out.print("일자 : ");
-			date = scan.nextInt();
+			try {
+				date = format1.parse(scan.nextLine());
+			} catch (ParseException e) {
+				System.out.println("잘못된 입력입니다.");
+			}
 			System.out.print("금액 : ");
 			money = scan.nextInt();
 			System.out.print("잔액 : ");
@@ -252,10 +279,14 @@ public class ABProgram implements Program{
 
 	/** 가계부 수정 2. 일자수정*/
 	private void runUpateInDate(int index) {
-		int date= 000000;
+		Date date=null;
 		try {		
 			System.out.print("일자 : ");
-			date = scan.nextInt();
+			try {
+				date = format1.parse(scan.nextLine());
+			} catch (ParseException e) {
+				System.out.println("잘못된 입력입니다.");
+			}
 		}catch(InputMismatchException e) {
 			System.out.println("잘못된 입력입니다.");
 			scan.nextLine();
@@ -265,6 +296,7 @@ public class ABProgram implements Program{
 
 	/** 가계부 수정 1. 입금/지출 항목수정*/
 	private void runUpateInOut(int index) {
+		//총액도 같이 변경
 		if(list.get(index).isInMoney() && !list.get(index).isOutMoney()) {
 			list.get(index).setInMoney(false);
 			list.get(index).setOutMoney(true);
@@ -278,9 +310,10 @@ public class ABProgram implements Program{
 	/** 가계부 삭제 : 양선진*/
 	private void deleteMoney() {
 		//순서대로 배열 나열해서 보기
-		for(Item tmp : list) {
-			tmp.toString();
+		if(!accountBookService.printAB(list)) {
+			System.out.println("조회에 실패했습니다.");
 		}
+		
 		//삭제할 번호 받기
 		System.out.print("삭제할 번호 : ");
 		//받은 num의 -1을 해야 인덱스 번호
@@ -297,6 +330,7 @@ public class ABProgram implements Program{
 		if(areYouSure == 'y') {
 			//리스트의 index 배열 삭제
 			list.remove(index);
+			System.out.println("삭제되었습니다.");
 			return;
 		} else { //y외 다른 문자면 취소
 			System.out.println("취소되었습니다.");
