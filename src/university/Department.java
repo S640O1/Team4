024269696351185ee87eeae7262399d71df30b8d
GyeongZ <sgj00700@naver.com>
2024-01-15 17:43:46 +0900

    SGJ : 학과 수정, 서비스 구성

--- src/university/Department.java
+++ src/university/Department.java
@@ -1,5 +1,9 @@
 package university;
 
+import java.io.Serializable;
+import java.util.List;
+import java.util.Objects;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,17 +13,41 @@ import lombok.Data;
 
 @Data
 @AllArgsConstructor
-public class Department {
+public class Department implements Serializable{
 	
+	private static final long serialVersionUID = -4403777244245105266L;
 	//학과 분류 번호
 	int dpNum;
 	//학과명
 	String dpName;
+	//학생리스트
+	List<Student> sList;
+	//교수리스트
+	List<Professor> pList;
 	
 	@Override
 	public String toString() {
 		return "Department [dpName=" + dpName + "]";
 	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Department other = (Department) obj;
+		return Objects.equals(dpName, other.dpName);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(dpName);
+	}
+	
+	
 
 
 }
