58af6e8c5b88f021fa2784b462afc31201d51271
Tomi606 <001dosirak@naver.com>
2024-01-16 23:19:52 +0900

    학생 구현 - 추가, 수정, 삭제

--- src/university/service/StudentService.java
+++ src/university/service/StudentService.java
@@ -1,5 +1,18 @@
 package university.service;
 
+import java.util.List;
+
+import university.Student;
+
 public interface StudentService {
+
+	//학생추가
+	public void addStudent(List<Student> list, Student student);
+	
+	//학생수정
+	public void setStudent(List<Student> list, Student student);
+
+	//학생삭제
+	public void deleteStudent(List<Student> list, Student student);
 
 }
--- src/university/service/StudentServiceImp.java
+++ src/university/service/StudentServiceImp.java
@@ -1,5 +1,56 @@
 package university.service;
 
+import java.util.List;
+import java.util.Scanner;
+
+import university.Student;
+
 public class StudentServiceImp implements StudentService {
+
+	Scanner sc = new Scanner(System.in);
+	
+	@Override
+	public void addStudent(List<Student> list, Student student) {
+		System.out.println("추가할 학생 정보를 입력하세요.");
+		System.out.println("학번 : ");
+		int studentId = sc.nextInt();
+		System.out.println("이름 : ");
+		sc.nextLine();
+		String name = sc.nextLine();
+		System.out.println("성별(1 : 여성, 2 : 남성) : ");
+		int gender = sc.nextInt();
+		//번호 잘못치면 return
+		if(gender != 1 || gender != 2) {
+			System.out.println("없는 번호입니다.");
+			return;
+		}
+		System.out.println("학과 : ");
+		sc.nextLine();
+		String department = sc.nextLine();
+		System.out.println("연락처 : ");
+		sc.nextLine();
+		String phoneNumber = sc.nextLine();
+
+		Student std = new Student(studentId, gender, name, department, phoneNumber);
+		
+		//이미있는 학생일때 추가 X (equals = 학번과 이름이 같을때)
+		if(list.equals(std)) {
+			System.out.println("이미있는 학생입니다.");
+			return;
+		}
+		//등록안되있으면 배열에 추가
+		list.add(student);
+		System.out.println("등록되었습니다.");
+	}
+
+	@Override
+	public void setStudent(List<Student> list, Student student) {
+		
+	}
+
+	@Override
+	public void deleteStudent(List<Student> list, Student student) {
+		
+	}
 
 }
