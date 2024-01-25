ba38584873c15b3c92d733ff3fd6e8ed7efaa221
sonnayeoung <sskss00@naver.com>
2024-01-25 18:01:49 +0900

    학과출력형태 변경

--- src/university/service/DPServiceImp.java
+++ src/university/service/DPServiceImp.java
@@ -115,6 +115,8 @@ public class DPServiceImp implements DPService {
 			System.out.println("등록된 학과가 없습니다.");
 			return false;
 		}
+		System.out.println(" 순번\t코드\t학과명");
+		System.out.println("---------------------------------------");
 		for(int i = 0; i<dList.size(); i++) {
 			System.out.println("[" +(i+1) + "] "+ dList.get(i).toString());
     	}
