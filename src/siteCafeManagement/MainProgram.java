6f7c3f64ad7c6e76e2197d9f3e2348db64fc1a2f
sonnayeoung <sskss00@naver.com>
2024-01-29 17:29:21 +0900

    카페 게시판 관리 패키지 생성 및 구조 설계

---
+++ src/siteCafeManagement/MainProgram.java
@@ -0,0 +1,25 @@
+package siteCafeManagement;
+
+import program.Program;
+
+public class MainProgram implements Program{
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void printMenu() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void runMenu(int menu) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/siteCafeManagement/siteCafeManagementMain.java
@@ -0,0 +1,30 @@
+package siteCafeManagement;
+
+public class siteCafeManagementMain {
+	
+	/*
+	 사이트 카페 관리 콘솔 프로그램
+	*DB 이용 필수(DB 학습 후 적용)*
+	
+	- 회원관리 (1) : 선진
+		- 회원가입
+		- 로그인
+		- 로그아웃
+	
+	- 관리자
+	    - 카테고리 등록/수정/삭제 (2) : 경재
+	    - 게시판 등록/수정/삭제	(3) : 아진
+	
+	- 사용자 (4) : 나영
+	    - 게시글 등록
+	    - 게시글 조회
+	    - 게시글 수정
+	    - 게시글 삭제
+	 
+	 */
+
+	public static void main(String[] args) {
+		
+	}
+
+}
