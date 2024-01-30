4ee1cd9279390ba807bcd67db15831fcbee95758
Tomi606 <001dosirak@naver.com>
2024-01-30 17:50:32 +0900

    회원가입 수정중

--- src/siteCafeManagement/membership/Membership.java
+++ src/siteCafeManagement/membership/Membership.java
@@ -19,7 +19,7 @@ public class Membership implements Serializable {
 	// 정규표현식 활용 어떻게 하는지 확인
 	//아이디, 비밀번호, 비밀번호 확인, 이름, 휴대폰 번호(010-1234-5678)
 	private String id, pw, pw2, name, phoneNumber;	
-	private int idNumber;	//주민등록번호 앞 8자리 ex)1998.10.09
+	private int idNumber;	//주민등록번호 앞 8자리 ex)981009-1,3(남자)/2,4(여자)
 	private char gender;	//성별
 	
 	public Membership(String id, String pw, String name, String phoneNumber, int idNumber, char gender) {
--- src/siteCafeManagement/membership/MembershipImp.java
+++ src/siteCafeManagement/membership/MembershipImp.java
@@ -3,6 +3,7 @@ package siteCafeManagement.membership;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 import siteCafeManagement.MainProgram;
 
@@ -22,22 +23,51 @@ public class MembershipImp implements MembershipInterface {
 		
 	}
 
+	/**
+	 * 회원가입 : 아이디, 비밀번호, 비밀번호 확인, 이름, 주민등록번호-성별(1,3남자/2,4여성), 전화번호
+	 */
 	@Override
 	public void join() {
 		//아이디, 비밀번호 정규표현식
-		String idRegex = "^[a-zA-Z0-9!@#$%^&*()]{8,12}$";
-		String pwRegex = "^[a-zA-Z0-9!@#$%^&*()]{8,15}+$";
+		String idRegex = "^[a-zA-Z0-9!@#$%^&*]{8,12}$";
+		String pwRegex = "^[a-zA-Z0-9!@#$%^&*]{8,15}$";
+		int index = 0;
 		
-		//아이디
-		System.out.print("");
+		System.out.print("생성할 아이디 입력 : ");
 		scan.nextLine();
+		String id = scan.nextLine();
+		//만약 있는 아이디이면 다른 아이디 입력
+		for(int i=0;i<memberShip.size();i++) {
+			if(memberShip.get(index).getId()) {
+				System.out.println("이미 있는 아이디입니다.");
+				return;
+			}
+		}
+		if(!Pattern.matches(idRegex, id)) {
+			System.out.println("8자~ 12자 영어 대 · 소문자, 숫자, 특수문자(!@#$%^&*)만 사용할 수 있습니다.");
+			return;
+		}			
+		
 		//비밀번호
-		System.out.print("");
-		scan.nextLine();
+		System.out.print("생성할 비밀번호 입력 : ");
+		String pw = scan.nextLine();
+		if(!Pattern.matches(pwRegex, pw)) {
+			System.out.println("8~15자리 영어 대 · 소문자, 숫자, 특수문자(!@#$%^&*)만 사용할 수 있습니다.");
+			return;
+		}
 		//비밀번호 확인
-		System.out.print("");
-		scan.nextLine();
+		String pwCheck = null;
+		do {
+			System.out.print("생성한 비밀번호 확인 : ");
+			pwCheck = scan.nextLine();
+			if(!pw.equals(pwCheck)) {
+				System.out.println("비밀번호를 다시 입력하세요");
+			}
+		}while(!pw.equals(pwCheck));
 		
+		//이름
+		System.out.print("이름 : ");
+		String name = scan.nextLine();
 	}
 
 }
