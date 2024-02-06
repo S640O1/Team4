904a893a750f54ef3e3840c3da5818ea43099be2
Tomi606 <001dosirak@naver.com>
2024-02-06 13:53:02 +0900

    user

--- src/cafe/controller/UserController.java
+++ src/cafe/controller/UserController.java
@@ -1,5 +1,28 @@
  package cafe.controller;
 
+import java.util.Scanner;
+
+import cafe.service.UserService;
+import cafe.service.UserServiceImp;
+
 public class UserController {
+
+	private Scanner sc;
+	private UserService userService;
+	
+	public UserController(Scanner sc) {
+		if(sc == null) {
+			sc = new Scanner(System.in);
+		}
+		this.sc = sc;
+		userService = new UserServiceImp();
+	}
+
+	/**
+	 * 회원가입
+	 */
+	public void join() {
+		
+	}
 
 }
