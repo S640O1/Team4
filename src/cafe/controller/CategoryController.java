bfa8070771b85175f70b4a1af330fe2e1de19605
GyeongZ <sgj00700@naver.com>
2024-02-14 19:06:47 +0900

    SGJ_카테고리 수정

--- src/cafe/controller/CategoryController.java
+++ src/cafe/controller/CategoryController.java
@@ -41,7 +41,7 @@ public class CategoryController {
 		System.out.println("1. 카테고리 등록");
 		System.out.println("2. 카테고리 수정");
 		System.out.println("3. 카테고리 삭제");
-		System.out.println("4. 카테고리 미리보기");
+		System.out.println("4. 카테고리 조회");
 		System.out.println("5. 뒤로가기");
 		System.out.println("-------------------");
 		System.out.println("메뉴 선택 : ");		
@@ -105,17 +105,7 @@ public class CategoryController {
 
         System.out.println("수정할 카테고리 이름을 입력하세요: ");
         String c_title = scan.next();
-        
-        
-        //c_num과 c_title을 따로 넘겨주어 수정 필요
-        /* 다음처럼 mapper작성 필요
-	       set
-				c_title = #{c_title}
-			where
-				c_num = #{c_num}
-         */
-
-
+      
         if (categoryService.updateCategory(c_num, c_title)) {
             System.out.println("카테고리를 수정하였습니다.");
         } else {
@@ -137,6 +127,7 @@ public class CategoryController {
 
 	/** 1. 카테고리 등록하는 메서드*/
 	private void insertCategory() {
+		
 		//정규표현식 체크
 		
 		System.out.println("카테고리 이름 : ");
