9ac2d05d03b6c8c9d0c1aaee4468c7f90a166007
GyeongZ <sgj00700@naver.com>
2024-02-16 11:40:41 +0900

    Squashed commit of the following:
    commit 5d29af33c33419ebf5ac8718cab8337f325d320e
    Merge: d07c718 8a20a96
    Author: S4O <153248138+S640O1@users.noreply.github.com>
    Date:   Fri Feb 16 11:34:34 2024 +0900
    
        Merge pull request #187 from S640O1/SNY
    
        .
    
    commit 8a20a9688033be50814ae45c4bb6539c48a6f733
    Author: sonnayeoung <sskss00@naver.com>
    Date:   Fri Feb 16 11:34:13 2024 +0900
    
        .

--- src/cafe/pagination/Criteria.java
+++ src/cafe/pagination/Criteria.java
@@ -1,9 +1,13 @@
 package cafe.pagination;
 
+import lombok.Data;
+
+@Data
 public class Criteria {
 
 	private int page; //현재 페이지
 	private int perPageNum; //한 페이지에 최대 컨텐츠 개수
+	private int pageStart;
 	
 	public Criteria() {
 		page = 1;
@@ -13,6 +17,7 @@ public class Criteria {
 	public Criteria(int page, int perPageNum) {
 		this.page = page;
 		this.perPageNum = perPageNum;
+		this.pageStart = getPageStart();
 	}
 	
 	public int getPageStart() {
